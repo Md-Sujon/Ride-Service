5aa073a54b3e89f99e7ab65a6bfdab21c7f5a8b9
Md-Sujon <mdsujon829562@gmail.com>
2021-03-19 23:59:28 +0600

    third

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from '../../firebaseConfig';
+import './Login.css'
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+ }else {
+    firebase.app(); 
+ }
+
 
 const Login = () => {
+
+  var googleprovider = new firebase.auth.GoogleAuthProvider();
+  const [user,setUser]=useState({
+      singedIn:false,
+      name:'',
+      email:'',
+      photo:''
+  })
+
+const haldeClickgoogle =()=>{
+
+    firebase.auth()
+  .signInWithPopup(googleprovider)
+  .then((result) => {
+    // var credential = result.credential;
+    // var token = credential.accessToken;
+    var user = result.user;
+    setUser(user)
+    console.log(user);
+
+
+  }).catch((error) => {
+    console.log(error);
+  });
+
+}
+
+
+
     return (
-        <div>
+        <div className="login">
             <h1>This is Login</h1>
+            <Button onClick={haldeClickgoogle} >Google Singin</Button>
         </div>
     );
 };
---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAdh5HdVwp8MW_VB3NEVeAdMM9JRn_rdaU",
+    authDomain: "trian-app.firebaseapp.com",
+    projectId: "trian-app",
+    storageBucket: "trian-app.appspot.com",
+    messagingSenderId: "14725341632",
+    appId: "1:14725341632:web:5a004caaba9ce4ed456825"
+  };
+
+  export default firebaseConfig;
